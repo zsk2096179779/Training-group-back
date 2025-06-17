f2578c5c3e6efa3569b4273cfb7a20c9c3104cc7
zsk2096179779 <147392590+zsk2096179779@users.noreply.github.com>
2025-06-17 13:20:13 +0800

    覆盖提交后端

---
+++ src/main/java/com/example/advisor_backend/AdvisorBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.advisor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AdvisorBackendApplication.class, args);
+	}
+}
+
